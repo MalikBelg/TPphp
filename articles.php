45ce27ccdca1447850f25c098ad5e6ad706fc5c0
MalikBelg <malik.belguermi@gmail.com>
2020-11-26 14:05:45 +0100

    Changement articles

--- articles.php
+++ articles.php
@@ -3,19 +3,24 @@
     $art1 = [ 
             "titre" => 'Le W3C vassalisé par les navigateurs web',
             "date" => '29 Mai 2019',
-            "contenu" => 'Normes web'
+            "contenu" => 'Le W3C a annoncé avoir confié l élaboration des normes HTML et DOM 
+            aux éditeurs de navigateurs web. Une victoire pour ces derniers, qui....'
     ];
         
     $art2 = [
-            "titre" => 'Le W3C publie des recommandations sur la normalisation des annotations Web',
-            "date" => '27 Février 2017',
-            "contenu" => 'Nouvelles normes'
+            "titre" => 'Salesforce pourrait racheter Slack dès la semaine prochaine',
+            "date" => '2 Novembre 2020',
+            "contenu" => 'D’après le Wall Street Journal, Salesforce est en pourparlers avec Slack pour 
+            un éventuel rachat. Si les deux firmes parviennent à un accord, l’annonce devrait être faite 
+            la semaine prochaine lorsque.....'
     ];
         
     $art3 = [
-            "titre" => 'Le HTML5 est officialisé en standard du web',
-            "date" => '29 Octobre 2014',
-            "contenu" => 'Standardisation HTML5'
+            "titre" => 'Microsoft dévoile une IA capable de décrire des images « aussi bien que les gens le font »',
+            "date" => '16 Octobre 2020',
+            "contenu" => 'Google a déjà proposé en 2016 une technologie similaire, mais cette fois-ci les équipes 
+            de Microsoft affirment qu’elles sont allées encore plus loin. Selon l’entreprise, ses chercheurs en 
+            intelligence artificielle ont mis au point un système....'
     ];     
     
 
